d74228360289f1c082d6a5b3eded61b30bb748d3
Аслан Нурсеитов <grooves97@gmail.com>
2019-04-19 10:53:55 +0600

    Создал модели

---
+++ SlockNote/SlockNote.Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlockNote.Models
+{
+    public abstract class Entity
+    {
+        Guid Id { get; set; }
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
